5a30d37ab7fdb75aff048a891c24ba7c23ad60ec
Daniel-Di-Fraia <daniel.difraia2600@gmail.com>
2026-03-30 15:09:55 +0200

    first commit

---
+++ snacks.js
@@ -0,0 +1,23 @@
+/* ===============================
+   ESERCIZI PRATICI
+   Scrivi il codice sotto ogni consegna
+   =============================== */
+
+/* 1) Confronta a e b (==) e stampa il risultato */
+let a = 5;
+let b = "5";
+console.log(a == b);
+
+/* 2) Confronta a e b (===) e stampa il risultato */
+console.log(a === b);
+
+/* 3) Stampa il tipo di x */
+let x;
+console.log(typeof(x));
+
+/* 4) Stampa il tipo di null */
+console.log(typeof null);
+
+/* 5) Stampa il risultato: 2 + "3" - 1 */
+console.log(2 + "3" -1);
+console.log(2 + Number("3") -1);
