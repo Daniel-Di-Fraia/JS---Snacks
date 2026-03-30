f26e955fbdceacb01d49fc1981a93f01bb4c36fa
Daniel-Di-Fraia <daniel.difraia2600@gmail.com>
2026-03-30 15:22:03 +0200

    snacks array

--- snacks.js
+++ snacks.js
@@ -21,3 +21,26 @@ console.log(typeof null);
 /* 5) Stampa il risultato: 2 + "3" - 1 */
 console.log(2 + "3" -1);
 console.log(2 + Number("3") -1);
+
+
+/* ---------- ARRAY ---------- */
+
+/* 6) Aggiungi 4 all’array e stampa la lunghezza */
+const arr1 = [1, 2, 3];
+arr1.push(4);
+console.log(arr1.length);
+
+/* 7) Stampa l’ultimo elemento dell’array */
+console.log(arr1[3]);
+
+/* 8) Crea un nuovo array con i valori raddoppiati */
+const doubledArray = arr1.map((val) => val * 2);
+console.log(doubledArray);
+
+/* 9) Crea un array con i numeri > 1 */
+const filtered = arr1.filter((val) => val > 1);
+console.log(filtered);
+
+/* 10) Somma tutti i valori dell’array */
+const sommaArray = arr1.reduce((accumulator, current) => accumulator + current, 0);
+console.log(sommaArray);
